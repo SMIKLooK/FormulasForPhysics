f3f22136917b6cdd6e0ac936a26eb1c86c21a8c5
SMIKLooK <hdhddby249@gmail.com>
2023-06-25 18:22:43 +0300

    Revert "Urgent bug fixes. Android Studio shows that there are no errors, but when launching the application, the formulas did not work, I had to change the code"
    This reverts commit 05e264c610e42b3c49e594db35603cc57d8c9901.

--- app/src/main/java/com/example/formulasforphysics/Class7Formulas.kt
+++ app/src/main/java/com/example/formulasforphysics/Class7Formulas.kt
@@ -3,93 +3,79 @@ package com.example.formulasforphysics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.zanvent.mathview.MathView
 
 class Class7Formulas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_class7_formulas)
 
-        val pixelScaleTypeScaleDP = MathView.Scale.SCALE_DP
-
-        createMathView(R.id.math_view70,
+        CreateMathView().createMathView(R.id.math_view70,
             getString(R.string.formula70),
             pixelScaleTypeScaleDP, 22)
 
 
-        createMathView(R.id.math_view71,
+        CreateMathView().createMathView(R.id.math_view71,
             getString(R.string.formula71),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view72,
+        CreateMathView().createMathView(R.id.math_view72,
             getString(R.string.formula72),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view73,
+        CreateMathView().createMathView(R.id.math_view73,
             getString(R.string.formula73),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view74,
+        CreateMathView().createMathView(R.id.math_view74,
             getString(R.string.formula74),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view75,
+        CreateMathView().createMathView(R.id.math_view75,
             getString(R.string.formula75),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view76,
+        CreateMathView().createMathView(R.id.math_view76,
             getString(R.string.formula76),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view77,
+        CreateMathView().createMathView(R.id.math_view77,
             getString(R.string.formula77),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view78,
+        CreateMathView().createMathView(R.id.math_view78,
             getString(R.string.formula78),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view79,
+        CreateMathView().createMathView(R.id.math_view79,
             getString(R.string.formula79),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view710,
+        CreateMathView().createMathView(R.id.math_view710,
             getString(R.string.formula710),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view711,
+        CreateMathView().createMathView(R.id.math_view711,
             getString(R.string.formula711),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view712,
+        CreateMathView().createMathView(R.id.math_view712,
             getString(R.string.formula712),
             pixelScaleTypeScaleDP, 20)
 
 
-        createMathView(R.id.math_view713,
+        CreateMathView().createMathView(R.id.math_view713,
             getString(R.string.formula713),
             pixelScaleTypeScaleDP,20)
     }
-    private fun createMathView(viewId: Int,
-                               formula: String,
-                               pixelScaleType: MathView.Scale,
-                               textSize: Int): MathView{
-        val mathView = findViewById<MathView>(viewId)
-        mathView.text = formula
-        mathView.pixelScaleType = pixelScaleType
-        mathView.setTextSize(textSize)
-        return mathView
-    }
-
 }
