35fe3ae426d743c32c94d85100e00af00893a857
SMIKLooK <hdhddby249@gmail.com>
2023-10-15 20:01:33 +0300

    fix bag's

--- app/src/main/java/com/example/formulasforphysics/MainActivity.kt
+++ app/src/main/java/com/example/formulasforphysics/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.formulasforphysics
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -21,42 +22,45 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.viewPager.adapter = VPAdapter(this, listMiddleClass)
-        TabLayoutMediator(binding.tb, binding.viewPager) {
-                tab, pos -> tab.text = listTextTB[pos]
-        }.attach()
-
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val toggle = ActionBarDrawerToggle(
-            this, binding.drawerLayout, toolbar, R.string.formula922t, R.string.formula922t)
+            this, binding.drawerLayout, toolbar, R.string.formula922t, R.string.formula922t
+        )
 
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+
+        toggle.drawerArrowDrawable.color = Color.WHITE
+
+        binding.viewPager.adapter = VPAdapter(this, listMiddleClass)
+        TabLayoutMediator(binding.tb, binding.viewPager) { tab, pos ->
+            tab.text = listTextTB[pos]
+        }.attach()
 
         binding.navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.middle_class -> {
                     binding.viewPager.adapter = VPAdapter(this, listMiddleClass)
-                    TabLayoutMediator(binding.tb, binding.viewPager) {
-                            tab, pos -> tab.text = listTextTB[pos]
+                    TabLayoutMediator(binding.tb, binding.viewPager) { tab, pos ->
+                        tab.text = listTextTB[pos]
                     }.attach()
 
                     binding.drawerLayout.closeDrawers()
-
                     true
                 }
+
                 R.id.top_class -> {
                     binding.viewPager.adapter = VPAdapter(this, listTopClass)
-                    TabLayoutMediator(binding.tb, binding.viewPager) {
-                            tab, pos -> tab.text = listTextTB[pos]
+                    TabLayoutMediator(binding.tb, binding.viewPager) { tab, pos ->
+                        tab.text = listTextTB[pos]
                     }.attach()
 
                     binding.drawerLayout.closeDrawers()
-
                     true
                 }
+
                 else -> false
             }
         }
--- app/src/main/java/com/example/formulasforphysics/data/app/AppData.kt
+++ app/src/main/java/com/example/formulasforphysics/data/app/AppData.kt
@@ -5,12 +5,15 @@ import com.example.formulasforphysics.MiddleClassLawsFragment
 import com.example.formulasforphysics.TopClassFragment
 import com.example.formulasforphysics.TopClassLawsFragment
 
+//для активити маин
 val listMiddleClass = listOf(MiddleClassFormulasFragment(), MiddleClassLawsFragment())
 val listTopClass = listOf(TopClassFragment(), TopClassLawsFragment())
 val listTextTB = listOf("формулы", "законы")
 
+//для фрагмента с формулами
 const val bundleKeyLaws = "numberL"
 const val fragmentBackStackLaws = "MiddleClassLawsFragment()"
 
+//для фрагмента с законами
 const val bundleKeyFormulas = "numberF"
 const val fragmentBackStackFormulas = "MiddleClassFormulasFragment"
